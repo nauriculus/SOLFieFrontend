81f26b09242102362c3f747cbafcdb026e56a23c
Nauri <24634581+nauriculus@users.noreply.github.com>
2024-10-22 13:51:20 +0200

    desktop v

--- src/app/mint/[mint]/page.jsx
+++ src/app/mint/[mint]/page.jsx
@@ -22,6 +22,7 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl } from "@solana/web3.js";
 require("@solana/wallet-adapter-react-ui/styles.css");
 import toast, { Toaster } from "react-hot-toast";
+import { useMediaQuery } from "react-responsive";
 
 function notify(props) {
   const { title, message, type } = props;
@@ -94,6 +95,7 @@ export default function MintPage({ params }) {
 }
 
 function MintComponent({ image, mint }) {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
   const wallet = useWallet();
   const [mintSuccess, setMintSuccess] = useState(false);
   const [mintLoading, setMintLoading] = useState(false);
@@ -139,7 +141,10 @@ function MintComponent({ image, mint }) {
   }
 
   return (
-    <div className="flex bg-white bg-center relative flex-col items-center pb-12 w-full min-h-[900px] max-md:px-5">
+    <div className="flex bg-white bg-center relative flex-col items-center pb-12 w-full min-h-[1400px] max-md:px-5">
+    
+    {isMobile ? (  
+      <div>
       <header className="absolute inset-x-0 top-0 z-50">
         <nav
           aria-label="Global"
@@ -209,7 +214,7 @@ function MintComponent({ image, mint }) {
                     <img
                       src={image}
                       alt="Captured"
-                      className="w-[100%] max-w-[1200px] rounded-lg"
+                      className="w-[90%] max-w-[1200px] rounded-lg"
                     />
                   </div>
                 )}
@@ -227,6 +232,7 @@ function MintComponent({ image, mint }) {
             </div>
           </div>
         </div>
+       
       </div>
 
       <Toaster
@@ -238,6 +244,114 @@ function MintComponent({ image, mint }) {
           },
         }}
       />
+      </div>
+       ) : (
+          <div>
+             <header className="absolute inset-x-0 top-0 z-50">
+            <nav
+              aria-label="Global"
+              className="flex items-center justify-between p-6 lg:px-8"
+            >
+              <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">SuperTeam</span>
+                  <img
+                    alt="SuperTeam"
+                    src="/images/superteam-ftext.png"
+                    className="h-12 w-auto object-cover object-center" // Adjusted for responsiveness
+                  />
+                </a>
+              </div>
+            </nav>
+          </header>
+
+            <div>
+
+      <div className="mx-aut flex w-full mt-[10%]">
+        <div className="bg-white">
+          <div className="p-10 rounded-lg">
+            <div className="relative h-[1100px] w-[100%] overflow-hidden bg-orange/90 px-5 pt-26 shadow-2xl rounded-lg">
+              <div className="mx-auto text-center">
+                <h2 className="text-5xl mt-[50px] font-bold tracking-tight text-white sm:text-4xl">
+                  Mint Your SOLFie
+                </h2>
+                <div className="flex items-center justify-center gap-x-6 lg:justify-start">
+                  {image && !mintSuccess && (
+                    <div className="mt-5 flex items-center justify-center gap-x-6">
+                      <WalletMultiButton />
+                      <button
+                        onClick={() => handleMint(wallet)}
+                        disabled={mintLoading}
+                        className={`flex items-center gap-2 rounded-md ${
+                          mintLoading ? "bg-gray-400" : "bg-white"
+                        } px-5 py-3.5 text-sm font-semibold text-black`}
+                      >
+                        {mintLoading ? (
+                          <ArrowPathIcon
+                            className="h-5 w-5 animate-spin"
+                            aria-hidden="true"
+                          />
+                        ) : (
+                          <GiftIcon className="h-5 w-5" aria-hidden="true" />
+                        )}
+                        {mintLoading ? "Minting..." : "Mint"}
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className="relative mt-16 h-80 lg:mt-8">
+                {mintSuccess && (
+                  <div className="flex ml-[10%] text-3xl text-white text-bold flex-col items-center mt-4 mb-10">
+                    <img
+                      src={"/images/success.gif"}
+                      alt="Captured"
+                      className="w-[100%] max-w-[900px] rounded-lg"
+                    />
+                    SOLFie minted sucessfully!
+                  </div>
+                )}
+
+                {image && !mintSuccess && (
+                  <div className="flex p-10 flex-col items-center mt-4 mb-10">
+                    <img
+                      src={image}
+                      alt="Captured"
+                      className="w-[55%] h-[55%] rounded-lg"
+                    />
+                  </div>
+                )}
+
+                {!image && !mintSuccess && (
+                  <div className="flex flex-col items-center mt-4 mb-10">
+                    <img
+                      src={"/images/loading.gif"}
+                      alt="Captured"
+                      className="w-[20%] rounded-lg"
+                    />
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+       
+      </div>
+
+      <Toaster
+        position={"bottom-center"}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            animation: "none",
+          },
+        }}
+      />
+      </div>
+
+            </div>
+        )}
     </div>
   );
 }
--- src/app/picture/index.jsx
+++ src/app/picture/index.jsx
@@ -8,13 +8,14 @@ import {
   ArrowUpCircleIcon,
   DocumentIcon,
   XMarkIcon,
-  CameraIcon,
 } from "@heroicons/react/24/outline";
+import { useMediaQuery } from "react-responsive";
 
 export default function TakePicturePage() {
-  const router = useRouter();
+  const isMobile = useMediaQuery({ maxWidth: 767 });
 
-  const [ratio, setRatio] = useState(30 / 30);
+  const router = useRouter();
+  const [ratio, setRatio] = useState(20 / 20);
   const [image, setImage] = useState(null);
   const camera = useRef(null);
   const [rotated, setRotated] = useState(false);
@@ -97,10 +98,6 @@ export default function TakePicturePage() {
     }
   };
 
-  const route = (uuid) => {
-    router.push("/mint/" + uuid.toString());
-  };
-
   const switchCamera = () => {
     if (camera.current) {
       camera.current.switchCamera();
@@ -113,152 +110,302 @@ export default function TakePicturePage() {
     }
   };
 
+  const route = (uuid) => {
+    router.push("/mint/" + uuid.toString());
+  };
+
   const { Canvas } = useQRCode();
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav
-          aria-label="Global"
-          className="flex items-center justify-between p-6 lg:px-8"
-        >
-          <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Superteam</span>
-              <img
-                alt=""
-                src="/images/superteam-ftext.png"
-                className="h-[100%] w-[50%] object-cover object-center"
-              />
-            </a>
-          </div>
-        </nav>
-      </header>
-
-      <div className="bg-white">
-        <div className="max-w-2xl sm:px-3 sm:py-32 lg:px-8">
-          <div className="relative h-[650px] isolate overflow-hidden bg-orange/90 px-2 pt-26 shadow-2xl sm:rounded-3xl sm:px-25 md:pt-14 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
-            <div className="mx-auto text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Take A Selfie
-              </h2>
-
-              {qrCode && (
-                <div className="flex mt-[2%] flex-col items-center mb-10">
-                  <Canvas
-                    text={"https://solfie.fun/mint/" + uuid}
-                    options={{
-                      errorCorrectionLevel: "M",
-                      margin: 3,
-                      scale: 4,
-                      width: 200,
-                      color: {
-                        dark: "#0c0706",
-                        light: "#FFBF60FF",
-                      },
-                    }}
+      {isMobile ? (
+        <div>
+          <header className="absolute inset-x-0 top-0 z-50">
+            <nav
+              aria-label="Global"
+              className="flex items-center justify-between p-6 lg:px-8"
+            >
+              <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">SuperTeam</span>
+                  <img
+                    alt=""
+                    src="/images/superteam-ftext.png"
+                    className="h-[100%] w-[50%] object-cover object-center"
                   />
+                </a>
+              </div>
+            </nav>
+          </header>
 
-                  <div
-                    className="mt-5 text-2xl text-white"
-                    onClick={() => route(uuid)}
-                  >
-                    Or Click Here!
+          <div className="bg-white">
+            <div className="max-w-2xl sm:px-3 sm:py-32 lg:px-8">
+              <div className="relative h-[650px] isolate overflow-hidden bg-orange/90 px-2 pt-26 shadow-2xl sm:rounded-3xl sm:px-25 md:pt-14 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+                <div className="mx-auto text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+                  <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Take A Selfie
+                  </h2>
+
+                  {qrCode && (
+                    <div className="flex mt-[2%] flex-col items-center mb-10">
+                      <Canvas
+                        text={"https://localhost:3000/mint/" + uuid}
+                        options={{
+                          errorCorrectionLevel: "M",
+                          margin: 3,
+                          scale: 4,
+                          width: 200,
+                          color: {
+                            dark: "#0c0706",
+                            light: "#fff",
+                          },
+                        }}
+                      />
+
+                      <div
+                        className="mt-10 text-2xl text-white"
+                        onClick={() => route(uuid)}
+                      >
+                        Or Click Here!
+                      </div>
+                    </div>
+                  )}
+
+                  <div className="flex items-center justify-center gap-x-6 lg:justify-start">
+                    {image ? (
+                      <div className="mt-5 flex items-center justify-center gap-x-6">
+                        <a
+                          onClick={reset}
+                          href="#"
+                          className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                        >
+                          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                          Reset
+                        </a>
+
+                        {!qrCode && (
+                          <a
+                            onClick={save}
+                            disabled={qrCode}
+                            href="#"
+                            className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                          >
+                            <DocumentIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                            Upload
+                          </a>
+                        )}
+                      </div>
+                    ) : (
+                      <div className="mt-5 flex items-center justify-center gap-x-6">
+                        <a
+                          onClick={capture}
+                          href="#"
+                          className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                        >
+                          <ArrowUpCircleIcon
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                          />
+                          Save
+                        </a>
+
+                        {!qrCode && (
+                          <a
+                            onClick={save}
+                            disabled={qrCode}
+                            href="#"
+                            className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                          >
+                            <DocumentIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                            Upload
+                          </a>
+                        )}
+                      </div>
+                    )}
                   </div>
                 </div>
-              )}
 
-              <div className="flex items-center justify-center gap-x-6 lg:justify-start">
-                {image ? (
-                  <div className="mt-5 flex items-center justify-center gap-x-6">
-                    <a
-                      onClick={reset}
-                      href="#"
-                      className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
-                    >
-                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                      Reset
-                    </a>
+                <div className="mx-auto mt-[10%] grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200" />
 
-                    {!qrCode && (
-                      <a
-                        onClick={save}
-                        disabled={qrCode}
-                        href="#"
-                        className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
-                      >
-                        <DocumentIcon className="h-5 w-5" aria-hidden="true" />
-                        Upload
-                      </a>
-                    )}
-                  </div>
-                ) : (
-                  <div className="mt-5 flex items-center justify-center gap-x-6">
-                    <a
-                      onClick={capture}
-                      href="#"
-                      className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
-                    >
-                      <ArrowUpCircleIcon
-                        className="h-5 w-5"
-                        aria-hidden="true"
+                <div className="relative mt-16 h-80 lg:mt-8">
+                  {!qrCode && !image && (
+                    <div>
+                      <Camera
+                        ref={camera}
+                        numberOfCamerasCallback={setNumberOfCameras}
+                        facingMode="user"
+                        aspectRatio={ratio}
                       />
-                      Save
-                    </a>
-
-                    {numberOfCameras > 1 && !image && !qrCode && (
-                      <a
-                        onClick={switchCamera}
-                        href="#"
-                        className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
-                      >
-                        <CameraIcon className="h-5 w-5" aria-hidden="true" />
-                        Switch
-                      </a>
-                    )}
-
-                    {!qrCode && (
-                      <a
-                        onClick={save}
-                        disabled={qrCode}
-                        href="#"
-                        className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
-                      >
-                        <DocumentIcon className="h-5 w-5" aria-hidden="true" />
-                        Upload
-                      </a>
-                    )}
-                  </div>
-                )}
+                    </div>
+                  )}
+                  {!qrCode && image && (
+                    <div className="flex flex-col items-center mt-4 mb-10">
+                      <img
+                        src={image}
+                        alt="Captured"
+                        className="w-[100%] max-w-[900px] rounded-lg"
+                      />
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
 
-            <div className="mx-auto mt-[10%] grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200" />
-
-            <div className="relative mt-16 h-80 lg:mt-8">
-              {!qrCode && !image && (
-                <div>
-                  <Camera
-                    ref={camera}
-                    numberOfCamerasCallback={setNumberOfCameras}
-                    facingMode="user"
-                    aspectRatio={ratio}
-                  />
-                </div>
-              )}
-              {!qrCode && image && (
-                <div className="flex flex-col items-center mt-4 mb-10">
-                  <img
-                    src={image}
-                    alt="Captured"
-                    className="w-[100%] max-w-[900px] rounded-lg"
-                  />
-                </div>
-              )}
-            </div>
+            {numberOfCameras > 1 && !image && !qrCode && (
+              <CameraIcon
+                onClick={switchCamera}
+                className="h-10 w-10"
+                aria-hidden="true"
+              />
+            )}
           </div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <header className="absolute inset-x-0 top-0 z-50">
+            <nav
+              aria-label="Global"
+              className="flex items-center justify-between p-6 lg:px-8"
+            >
+              <div className="flex lg:flex-1">
+                <a href="#" className="-m-1.5 p-1.5">
+                  <span className="sr-only">SuperTeam</span>
+                  <img
+                    alt="SuperTeam"
+                    src="/images/superteam-ftext.png"
+                    className="h-12 w-auto object-cover object-center" // Adjusted for responsiveness
+                  />
+                </a>
+              </div>
+            </nav>
+          </header>
+
+          <div className="bg-white">
+            <div className="full-w p-10 mt-[10%] rounded-lg">
+              <div className="relative isolate overflow-hidden bg-orange/90 px-4 py-12 shadow-2xl rounded-lg">
+                <div className="mx-auto text-center lg:mx-0 lg:flex-auto lg:py-16 lg:text-left">
+                  <h2 className="text-5xl font-bold tracking-tight text-white sm:text-4xl md:text-5xl">
+                    Take A Selfie
+                  </h2>
+
+                  {qrCode && (
+                    <div className="flex mt-6 flex-col items-center mb-8">
+                      <Canvas
+                        text={"https://localhost:3000/mint/" + uuid}
+                        options={{
+                          errorCorrectionLevel: "M",
+                          margin: 3,
+                          scale: 4,
+                          width: 250,
+                          color: {
+                            dark: "#0c0706",
+                            light: "#fff",
+                          },
+                        }}
+                      />
+                    </div>
+                  )}
+
+                  <div className="flex items-center justify-center gap-x-6 lg:justify-start">
+                    {image ? (
+                      <div className="mt-5 flex items-center justify-center gap-x-10">
+                        <a
+                          onClick={reset}
+                          href="#"
+                          className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                        >
+                          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+                          Reset
+                        </a>
+
+                        {!qrCode && (
+                          <a
+                            onClick={save}
+                            disabled={qrCode}
+                            href="#"
+                            className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                          >
+                            <DocumentIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                            Upload
+                          </a>
+                        )}
+                      </div>
+                    ) : (
+                      <div className="mt-5 flex items-center justify-center gap-x-6">
+                        <a
+                          onClick={capture}
+                          href="#"
+                          className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                        >
+                          <ArrowUpCircleIcon
+                            className="h-5 w-5"
+                            aria-hidden="true"
+                          />
+                          Save
+                        </a>
+
+                        {!qrCode && (
+                          <a
+                            onClick={save}
+                            disabled={qrCode}
+                            href="#"
+                            className="flex items-center gap-2 rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black"
+                          >
+                            <DocumentIcon
+                              className="h-5 w-5"
+                              aria-hidden="true"
+                            />
+                            Upload
+                          </a>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+
+                <div className="mx-auto mt-10 max-w-xl lg:mt-0">
+                  {!qrCode && !image && (
+                    <div>
+                      <Camera
+                        ref={camera}
+                        numberOfCamerasCallback={setNumberOfCameras}
+                        facingMode="user"
+                        aspectRatio={ratio}
+                      />
+                    </div>
+                  )}
+                  {!qrCode && image && (
+                    <div className="flex flex-col items-center mt-4">
+                      <img
+                        src={image}
+                        alt="Captured"
+                        className="w-full rounded-lg" // Adjusted for responsiveness
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            {numberOfCameras > 1 && !image && !qrCode && (
+              <CameraIcon
+                onClick={switchCamera}
+                className="h-10 w-10"
+                aria-hidden="true"
+              />
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
